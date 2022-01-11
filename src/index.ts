db478987007faf67750406d437c03b841b7576cd
marco-prado <marcoacop@live.com>
2022-01-11 15:56:18 -0300

    feat: add testing and dev environments and createUser mutation test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { setupServer, setupDatabase } from './setup';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function main() {
   await setupDatabase();
@@ -6,7 +9,7 @@ async function main() {
     await setupServer(4000);
     console.log('Server is ready at http://localhost:4000/graphql');
   } catch (err) {
-    console.error("Error starting the node server", err);
+    console.error('Error starting the node server', err);
   }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,11 @@
 import { setupDatabase, setupServer } from '../src/setup';
 import * as request from 'supertest';
 import { expect } from 'chai';
+import * as dotenv from 'dotenv';
+import { getConnection } from 'typeorm';
+import { User } from '../src/entity/User';
+
+dotenv.config({ path: __dirname + '/../test.env' });
 
 before(async () => {
   await setupDatabase();
@@ -12,4 +17,41 @@ describe('Hello test', () => {
     const res = await request('localhost:4000').post('/graphql').send({ query: '{hello}' });
     expect(res.body.data.hello).to.be.eq('Hello, world!');
   });
+});
+
+describe('createUser test', () => {
+  it('createUser mutation', async () => {
+    const res = await request('localhost:4000')
+      .post('/graphql')
+      .send({
+        query: `
+        mutation {
+          createUser(
+            data: { name: "Marco", email: "marco@email.com", password: "pswd123", birthDate: "04-01-2000" }
+          ) {
+            id
+            name
+            email
+            birthDate
+          }
+        }
+      `,
+      });
+    expect(res.body.data.createUser.id).to.exist;
+    expect(res.body.data.createUser.name).to.be.eq('Marco');
+    expect(res.body.data.createUser.email).to.be.eq('marco@email.com');
+    expect(res.body.data.createUser.birthDate).to.be.eq('954558000000');
+
+    const userRepository = getConnection().getRepository(User);
+    const dbUser = await userRepository.findOne(res.body.data.createUser.id);
+    expect(dbUser.name).to.be.eq('Marco');
+    expect(dbUser.email).to.be.eq('marco@email.com');
+    expect(dbUser.birthDate.toDateString()).to.be.eq(new Date('2000-04-01T03:00:00.000Z').toDateString());
+    expect(dbUser.password).to.not.be.eq('pswd123');
+  });
+});
+
+afterEach(async () => {
+  const userRepository = getConnection().getRepository('User');
+  await userRepository.clear();
 });
